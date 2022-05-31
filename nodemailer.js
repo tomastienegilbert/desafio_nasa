07fe0fd00a193fde040f9efef3fd4ed0f441d9eb
tomastienegilbert <68029999+tomastienegilbert@users.noreply.github.com>
2022-05-31 10:50:58 -0400

    Update nodemailer.js

--- nodemailer.js
+++ nodemailer.js
@@ -9,16 +9,38 @@ let transporter = nodemailer.createTransport({
     },
 });
 
-// Función asíncrona "send()" que recibe el email y el nombre 
-const send = async (email, nombre) => {
-    let mailOptions = {
+//5 Al guardar con éxito una imagen se le debe mostrar al usuario un mensaje de agradecimiento por haber colaborado con la NASA.
+const correoAgradecimiento = async (email, nombre) => {
+    let configCorreo = {
         from: 'chileinfoclub@gmail.comm',
         to: [email],
-        subject: `¡Saludos desde la NASA!`,
-        html: `<h3> ¡Hola!, ${nombre} <br> La NASA te da las gracias por subir tu foto en nuestro sistema y colaborar con nuestras investigaciones </h3>`,
+        subject: `NASA: ¡Gracias por tu aporte!`,
+        html: `<h3> ¡Hola ${nombre}! <br> Gracias por tu aporte a la NASA </h3>`,
     };
-    await transporter.sendMail(mailOptions)
+    await transporter.sendMail(configCorreo)
 };
 
-// exportar la función send
-module.exports = send;
+//6) Enviar un correo electrónico a los usuarios cuando se registren indicando que pasarán a revisión y serán notificados cuando se hayan validado sus credenciales.
+const correoRegistro = async (email, nombre) => {
+    let configCorreo = {
+        from: 'chileinfoclub@gmail.comm',
+        to: [email],
+        subject: `NASA: ¡Gracias por tu registro!`,
+        html: `<h3> ¡Hola ${nombre}! <br> Gracias por registrarte en la NASA. pronto validaremos tu cuenta para que puedas acceder y aportar con evidencias </h3>`,
+    };
+    await transporter.sendMail(configCorreo)
+};
+
+//7) Enviar un correo electrónico a los usuarios cuando sean validados en la vista “Admin” notificando que ahora tienen permiso de subir imágenes al sistema.
+const correoValidacion = async (email, nombre) => {
+    let configCorreo = {
+        from: 'chileinfoclub@gmail.comm',
+        to: [email],
+        subject: 'NASA: ¡Validamos tu cuenta!',
+        html: `<h3> ¡Hola ${nombre}! <br> Validamos tu cuenta en la NASA. ahora puedes subir evidencias </h3>`,
+    };
+    await transporter.sendMail(configCorreo)
+};
+
+// exportar funciones de envío
+module.exports = {correoAgradecimiento, correoRegistro, correoValidacion};
